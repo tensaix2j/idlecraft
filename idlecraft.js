41b95fa557a5fb5d5c295f7bf1c55cd84a510cca
tensaix2j <tensaix2j@gmail.com>
2015-03-02 22:19:13 +0800

    modified:   idlecraft.css 	modified:   idlecraft.js

--- idlecraft.js
+++ idlecraft.js
@@ -7,18 +7,23 @@ function Idlecraft() {
 	this.setting_price_base 	 	 	= [ 4.00 , 60.0 , 720 ,  8640,        83680,      1044160,      10929920,      179159040,      4149000000,      65798000000  ];
 	this.setting_price_delta 			= [ 0.28 , 9.00 , 115 ,  1440,        14183,       180027,       1917529,       31992685,       754363636,      12184814814  ]; 
 	this.setting_price_delta2  			= [ 0.02 , 1.35 , 18.3,   240,         2403,        31039,        336408,        5712979,       137157024,       2256447187  ];
+	this.setting_price_delta3 			= [ 0.02 , 0.04 , 0.08,  0.16,         0.32,         0.64,          1.28,           2.56,            5.12,            10.24  ];
 	this.setting_duration      			= [  500 , 2500 , 5000,   11000,      23000,        47000,         95000,         191000,          383000,           767000  ];
 	this.setting_managerprice 			= [ 1000, 15000, 100000, 500000,    4200000,     40000000,     870000000,    14000000000,    280000000000,    7000000000000  ];
+	this.timerinterval 		= 20;
 	
+
 	// Variables
 	this.generate_profits 	= [];
 	this.prices   		  	= [];
 	this.prices_delta		= [];
+	this.prices_delta2   	= [];
+	this.durations 			= [];
 	this.durationremaining 	= [];
 	this.itemactivated 		= [];
 	this.managerhired 		= [];
 	this.balance 			= 0.00;
-
+	this.durationbarlength  = 200;
 
 	
 	//---------
@@ -70,11 +75,11 @@ function Idlecraft() {
 				var itemid = row * 2 + col;
 
 				strhtml += "<div class='ictcol'>";
-					strhtml += "<div class='ictable' id='ictable" + itemid + "'  style='display:none;' >";
+					strhtml += "<div class='icsubtable' id='ictable" + itemid + "'  style='display:none;' >";
 
 							
 					strhtml += "<div class='ictrow'>";
-						strhtml += "<div class='ictcol1'>Item " + itemid +"</div>";
+						strhtml += "<div class='ictcol1'>Item " + itemid +"<div id='c1"+ itemid +"' ></div></div>";
 						strhtml += "<div class='ictcol2' id='c2"+ itemid +"'>"
 							strhtml += "<div id='c2lbl"+ itemid +"'></div>";
 							strhtml += "<div class='durationbar' id='durationbar"+ itemid +"'></div>";
@@ -106,8 +111,10 @@ function Idlecraft() {
 
 				this.prices[itemid] 		  				= this.setting_price_base[itemid];
 				this.prices_delta[itemid] 					= this.setting_price_delta[itemid]; 
+				this.prices_delta2[itemid] 					= this.setting_price_delta2[itemid];
 				this.generate_profits[itemid] 				= this.setting_generate_multiplier[itemid];
-				this.durationremaining[itemid] 				= this.setting_duration[ itemid ];	
+				this.durations[itemid] 						= this.setting_duration[itemid] ;
+				this.durationremaining[itemid] 				= 0;	
 				this.managerhired[itemid] 					= 0;
 
 				if ( itemid == 0 ) {
@@ -124,6 +131,12 @@ function Idlecraft() {
 		$("#ictable").append(strhtml);
 	}
 
+	//-----
+	this.debug = function() {
+
+		
+	}
+
 
 	//-----------
 	this.formatcurrency = function( currency ) {
@@ -134,7 +147,7 @@ function Idlecraft() {
 			return ( currency / 1000000 ).toFixed(3) + " Million";
 		}
 
-		return currency.toFixed(3);
+		return currency.toFixed(2);
 	}
 
 	//---------
@@ -144,14 +157,14 @@ function Idlecraft() {
 
 			if ( this.itemactivated[i] > 0 ) {
 				if ( this.durationremaining[ i ] > 0 ) {
-					this.durationremaining[ i ] -= 100 ;
+					this.durationremaining[ i ] -= this.timerinterval ;
 					
 					if ( this.durationremaining[i] <= 0 ) {
 						this.durationremaining[i] = 0;
 						this.balance += this.generate_profits[i];
 					
 						if ( this.managerhired[i] == 1 ) {
-							this.durationremaining[i] = this.setting_duration[i];
+							this.durationremaining[i] = this.durations[i];
 						}
 					}
 
@@ -168,10 +181,11 @@ function Idlecraft() {
 		var ic = this;
 		this.createdom();
 		this.bindclickevent();
+		this.debug();
 
 		setTimeout( function() {
 			ic.on_timer();
-		},100 );
+		},this.timerinterval );
 
 	}	
 
@@ -179,7 +193,7 @@ function Idlecraft() {
 	this.item_generate_onclick = function( itemid ) {
 
 		if ( this.durationremaining[itemid] == 0 ) {
-			this.durationremaining[itemid] = this.setting_duration[itemid];
+			this.durationremaining[itemid] = this.durations[itemid];
 		}
 	}
 
@@ -194,7 +208,7 @@ function Idlecraft() {
 				this.managerhired[itemid] = 1;
 
 				if ( this.durationremaining[itemid] <= 0 ) {
-					this.durationremaining[itemid] = this.setting_duration[itemid];
+					this.durationremaining[itemid] = this.durations[itemid];
 				}	
 			}
 		}
@@ -211,9 +225,21 @@ function Idlecraft() {
 			this.generate_profits[itemid] = this.setting_generate_multiplier[itemid] * this.itemactivated[itemid];
 			
 			this.prices[itemid] 		  += this.prices_delta[itemid];
-			this.prices_delta[itemid]     += this.setting_price_delta2[itemid];
+			this.prices_delta[itemid]     += this.prices_delta2[itemid];
+			this.prices_delta2[itemid]    += this.setting_price_delta3[itemid];
 			
-			
+			// Double speed at 25th, 50th,100th upgrade..
+			if ( this.itemactivated[itemid]  % 25 == 0 ) {
+				var a = this.itemactivated[itemid] / 25;
+				if ( (a & (a - 1)) == 0) {
+					this.durations[itemid] = (this.durations[itemid] / 2) >> 0;
+					if ( this.durations[ itemid ] < 1 ) {
+						this.durations[itemid] = 1;
+					}
+
+				}
+			}
+
 		}	
 
 	}
@@ -227,18 +253,24 @@ function Idlecraft() {
 
 				$("#ictable" + i).show();
 
+				$("#c1"+i).html( "Time :" + this.durations[i] );
+
 				if ( this.durationremaining[i] > 0 ) {
 					$("#c2lbl"+i).html( "Generate: $ " + this.formatcurrency( this.generate_profits[i] ) + "<br/>" +  ( this.durationremaining[i]/1000 ).toFixed(3) + "s remaining");
 				} else {
 					$("#c2lbl"+i).html( "Generate: $ " + this.formatcurrency( this.generate_profits[i] ) );
 				}
 
-				var length_in_pixel = ( this.durationremaining[i] * 208 / this.setting_duration[i] ) >> 0;
-				
+				if ( this.durations[i] <= 100 ) {
+					var length_in_pixel = this.durationbarlength;
+				} else {
+					var length_in_pixel = ( this.durationremaining[i] * this.durationbarlength / this.durations[i] ) >> 0;
+				}
+
 				$("#durationbar" + i ).css("width", length_in_pixel + "px");
 				
-				$("#c3"+i).html(  this.itemactivated[i] );
-				$("#c4"+i).html( "Upgrade : $ " + this.formatcurrency( this.prices[i] ) );
+				$("#c3"+i).html( "Qty : " + this.itemactivated[i] );
+				$("#c4"+i).html( "Buy : $ " + this.formatcurrency( this.prices[i] ) );
 				
 				if ( this.managerhired[i] == 1 ){
 					$("#c6"+i).html("Manager Hired");
@@ -271,7 +303,7 @@ function Idlecraft() {
 		
 		setTimeout( function() {
 			ic.on_timer();
-		},100 );
+		},this.timerinterval );
 	}
 
 	
